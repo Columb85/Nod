4cefe2973e6b9b978427fc2d290e482bd42dc90a
Columb <140189223+Columb85@users.noreply.github.com>
2024-11-28 16:51:24 +0200

    Update proxy_manager.py

--- core/utils/proxy_manager.py
+++ core/utils/proxy_manager.py
@@ -2,6 +2,35 @@ import asyncio
 from collections import deque
 from better_proxy import Proxy
 from core.utils.file_manager import file_to_list
+import random
+from queue import Queue
+
+class ProxyPool:
+    def __init__(self, proxy_file):
+        self.proxy_queue = Queue()
+        self.load_proxies_from_file(proxy_file)
+
+    def load_proxies_from_file(self, proxy_file):
+        try:
+            with open(proxy_file, 'r') as file:
+                for line in file:
+                    proxy = line.strip()
+                    if proxy:
+                        self.proxy_queue.put(proxy)
+        except FileNotFoundError:
+            print(f"Ошибка: Файл {proxy_file} не найден!")
+
+    def get_proxy(self):
+        if self.proxy_queue.empty():
+            print("Все прокси использованы. Перезагружаем пул.")
+            return None
+        return self.proxy_queue.get()
+
+    def return_proxy(self, proxy):
+        self.proxy_queue.put(proxy)
+
+    def size(self):
+        return self.proxy_queue.qsize()
 
 proxies = deque()
 
